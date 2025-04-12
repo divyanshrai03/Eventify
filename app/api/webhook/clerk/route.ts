aa57312eeeeddfdc820b980b5b2af4f5a01829e9
Divyansh Rai <ce.divyanshrai@gmail.com>
2025-04-12 10:56:41 +0530

    Initial commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,8 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs'
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
+import { createUser,  updateUser, deleteUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
@@ -16,9 +15,10 @@ export async function POST(req: Request) {
  
   // Get the headers
   const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  const svix_id = (await headerPayload).get("svix-id");  ///check for errors 
+
+  const svix_timestamp = (await headerPayload).get("svix-timestamp");
+  const svix_signature = (await headerPayload).get("svix-signature");
  
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -61,19 +61,21 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || "",
+      lastName: last_name || "",
       photo: image_url,
-    }
+    };
 
     const newUser = await createUser(user);
 
+    //////////////////////////possible errrorrrrrrr
     if(newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
+      const clerk = await clerkClient();
+      await clerk.users.updateUserMetadata(id, {
         publicMetadata: {
-          userId: newUser._id
-        }
-      })
+          userId: newUser._id,
+        },
+      });
     }
 
     return NextResponse.json({ message: 'OK', user: newUser })
@@ -83,8 +85,8 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || "",
+      lastName: last_name || "" ,
       username: username!,
       photo: image_url,
     }
@@ -104,4 +106,3 @@ export async function POST(req: Request) {
  
   return new Response('', { status: 200 })
 }
- 
