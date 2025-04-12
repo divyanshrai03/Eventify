48147f9bf514476845155382bb50155a78f9e546
Divyansh Rai <ce.divyanshrai@gmail.com>
2025-04-12 10:30:51 +0530

    add env

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,6 +1,6 @@
 import { IEvent } from '@/lib/database/models/event.model'
 import { formatDateTime } from '@/lib/utils'
-import { auth } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -12,8 +12,8 @@ type CardProps = {
   hidePrice?: boolean
 }
 
-const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
-  const { sessionClaims } = auth();
+const Card = async({ event, hasOrderLink, hidePrice }: CardProps) => {
+  const { sessionClaims } = await auth();
   const userId = sessionClaims?.userId as string;
 
   const isEventCreator = userId === event.organizer._id.toString();
