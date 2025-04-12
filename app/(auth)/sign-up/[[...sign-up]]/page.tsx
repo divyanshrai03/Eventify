c26b04edc10dfb75406198c05c625f9044854344
Divyansh Rai <ce.divyanshrai@gmail.com>
2025-04-12 12:01:28 +0530

    signup

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
-<<<<<<< HEAD
 export default function Page() {
   return <SignUp />
-=======
-export default function Page(){
-    return <SignUp />
->>>>>>> 40c7d2a0401aa14dcd0d3ea19f8ef367f5fb37c9
 }
